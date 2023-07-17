a156e776808d73950ec3753402810275d06d67aa
NY <131418857+network-yard@users.noreply.github.com>
2023-07-17 12:18:21 +0900

    Add files via upload

---
+++ simulate.js
@@ -0,0 +1,8 @@
+var w = $('.result1').width();
+var h = $('.result1').height();
+$('.result1').attr('width',w);
+$('.result1').attr('height',h);
+var w2 = $('.result2').width();
+var h2 = $('.result2').height();
+$('.result2').attr('width',w2);
+$('.result2').attr('height',h2);
